a438613ef4cafd39b82cb5d1b1876fd5a7b29ee1
VisualVibe <Visual.vibe1999@gmail.com>
2025-11-23 09:59:01 +0100

    Add UrlContext and refactor Blocker component to remove unused imports

--- src/components/Headless/Blocker.jsx
+++ src/components/Headless/Blocker.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import PropTypes from 'prop-types';
 import useBlockUrl from '../../shared/hooks/useBlockUrl';
 const Blocker = ({BlockedItem , isRunning}) => {
     useBlockUrl(BlockedItem , isRunning);
